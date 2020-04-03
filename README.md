46b69796209c2e6b1447dcf2f6c9e08d42b0ced8
Mihai Dumitrescu <mdumitrescu@testo.de>
2020-04-03 22:47:57 +0200

    start working on chapter 3

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# first-project
+# Haskel Study Project
--- test/MaybeMonSpec.hs
+++ test/MaybeMonSpec.hs
@@ -1,14 +1,19 @@
-import Lib
-import Test.Hspec
+module MaybeMonSpec where
+
+import           Lib
+import           Test.Hspec
 
 main :: IO ()
+main = hspec spec
 
-main = hspec $ do
-    describe "Working with monads" $ do
-     it "differs between Just and Nothing" $ do
-       _lift (3 :: Int) `shouldBe` (JustMon(3))
-
-
-
-
-
+spec :: Spec
+spec =
+  describe "Working with monads" $ do
+    it "differs between Just and Nothing" $ do
+      _lift 3 `shouldBe` JustMon (3 :: Int)
+      _lift 3 `shouldNotBe` NothingMon
+    it "correctly binds between containers" $ do
+      _lift 3 Lib.>>= (\x -> _lift (x + 3)) `shouldBe` JustMon 6
+      _lift 3 Lib.>>= (\x -> _lift (x ^ x)) `shouldBe` JustMon 27
+      NothingMon Lib.>>= (\x -> _lift (x ^ x)) `shouldBe` NothingMon
+    it "can add a number in a Monad" $ 3 `plus` (_lift 4) `shouldBe` JustMon 7
